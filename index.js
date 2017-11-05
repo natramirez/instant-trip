edf02a28a24a03e35aa794041df9ba884f3628ad
Natalia Ramirez <natalia@nat-oitwireless-inside-vapornet100-b-27683.Princeton.EDU>
2017-11-05 16:10:39 -0500

    Fixed port number issue for Heroku

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use('/api', apiRoutes);
 
 
 // start the server
+var port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
