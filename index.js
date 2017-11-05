ca6811519d7a807eb441d2893b72b8237a93b4bd
Natalia Ramirez <natalia@nat-oitwireless-inside-vapornet100-b-27683.Princeton.EDU>
2017-11-05 16:14:37 -0500

    Port number issue update

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.use('/api', apiRoutes);
 
 // start the server
 var port = process.env.PORT || 3000;
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
+app.listen(port, () => {
+  console.log(`Server is running on port number ${port}`);
 });
